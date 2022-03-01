acd2b36fbd205ef2512553da717da6e9d42b82e9
Dillon Smedley <smedleydillon@gmail.com>
2022-02-28 20:44:31 -0500

    Fix test

--- src/layouts/AppLayout/AppLayout.test.tsx
+++ src/layouts/AppLayout/AppLayout.test.tsx
@@ -3,6 +3,13 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import AppLayout from './AppLayout';
 
+const mockUseNavigate = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: () => mockUseNavigate,
+}));
+
 describe('<AppLayout />', () => {
   it('should mount', () => {
     render(<AppLayout />);
